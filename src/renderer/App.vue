5cd2d9437c18c482b9160c0e25555660b367bcba
ariyn <himnowxz@gmail.com>
2019-10-01 15:25:16 +0900

    electron-vue init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ppomo-git',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
